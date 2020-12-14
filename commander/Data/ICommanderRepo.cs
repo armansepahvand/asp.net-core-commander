5ae48f0d23b10089568cd144669d32a7f2ce236c
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2020-12-13 18:15:27 -0700

    Created an IRepository file

---
+++ commander/Data/ICommanderRepo.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using commander.Models;
+
+namespace commander.Data
+
+{
+    public interface  ICommanderRepo
+    {
+        IEnumerable<Command> GetAppCommands();
+        Command GetCommandById(int id);
+    }
+}
