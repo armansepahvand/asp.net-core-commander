f5b7bf0215db7c22407780a2286aff8ed5f31bec
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2020-12-13 18:06:33 -0700

    created a model for our commands

---
+++ commander/Models/Command.cs
@@ -0,0 +1,12 @@
+namespace commander.Models
+{
+    //create a model for our commands
+    public class Command
+    {
+        public int Id { get; set; }
+        public string HowTo { get; set; }
+        public string Line { get; set; }
+        public string Platform { get; set; }
+
+    }
+}
