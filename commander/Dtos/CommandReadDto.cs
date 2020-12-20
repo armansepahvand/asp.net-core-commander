cae3eef7211f98e31a1c28806b76e5c53d355a58
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2020-12-19 23:01:26 -0700

    Added CommandReadTdo

---
+++ commander/Dtos/CommandReadDto.cs
@@ -0,0 +1,19 @@
+namespace Commander.Dtos
+{
+    //create a model for our commands
+    public class CommandReadDto
+    {
+        
+        public int Id { get; set; }
+
+        
+        public string HowTo { get; set; }
+
+       
+        public string Line { get; set; }
+
+        
+        //public string Platform { get; set; }
+
+    }
+}
