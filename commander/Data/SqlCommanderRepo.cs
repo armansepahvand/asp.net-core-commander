794d257c360c714fc17b57ff4fd4208a85de4eb3
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2020-12-26 12:03:40 -0700

    Added CreateCommand and SaveChanges Methods

--- commander/Data/SqlCommanderRepo.cs
+++ commander/Data/SqlCommanderRepo.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Commander.Models;
@@ -9,18 +10,33 @@ namespace Commander.Data
         private readonly CommanderContext _context;
 
         public SqlCommanderRepo(CommanderContext context)
-           {
-               _context=context;
-           }
+        {
+            _context = context;
+        }
+
+        public void CreateCommand(Command cmd)
+        {
+            if (cmd == null)
+            {
+                throw new ArgumentNullException(nameof(cmd));
+            }
+            _context.Commands.Add(cmd);
+
+        }
 
         public IEnumerable<Command> GetAllCommands()
         {
-           return _context.Commands.ToList();
+            return _context.Commands.ToList();
         }
 
         public Command GetCommandById(int id)
         {
-           return _context.Commands.FirstOrDefault(p => p.Id == id );
+            return _context.Commands.FirstOrDefault(p => p.Id == id);
+        }
+
+        public bool SaveChanges()
+        {
+            return (_context.SaveChanges() >= 0);
         }
     }
 }
