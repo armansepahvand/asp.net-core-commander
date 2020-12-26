f3d78472859faa4b593ce69f5e42b4f0f9c53f9d
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2020-12-26 11:47:43 -0700

    Added CreateCommand and SaveChanges methods to CommanderRepo Interface

--- commander/Data/ICommanderRepo.cs
+++ commander/Data/ICommanderRepo.cs
@@ -6,9 +6,12 @@ namespace Commander.Data
 {
     public interface  ICommanderRepo
     {
+        bool SaveChanges();
         IEnumerable<Command> GetAllCommands();
         Command GetCommandById(int id);
 
+        void CreateCommand(Command cmd);
 
     }
+
 }
