a8307e5b45d0d0ecad44a61a11c2bfc5d3b34ea0
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2020-12-26 14:15:00 -0700

    Added the same constraints we had in Command model to CommandcreateDto

--- commander/Dtos/CommandCreateDto.cs
+++ commander/Dtos/CommandCreateDto.cs
@@ -1,16 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Commander.Dtos
 {
     //create a model for our commands
     public class CommandCreateDto
     {
 
-
+        [Required]
+        [MaxLength(250)]
         public string HowTo { get; set; }
 
-
+        [Required]
         public string Line { get; set; }
 
-
+        [Required]
         public string Platform { get; set; }
 
     }
